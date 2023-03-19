1e0563c7456aa015788ff55dc6b05cb735bcb333
Vinhnd <92012621+vnMarcus@users.noreply.github.com>
2023-03-20 01:04:54 +0700

    new db

--- db/dbERD.sql
+++ db/dbERD.sql
@@ -1,11 +1,12 @@
-CREATE TABLE `user` (id bigint(19) NOT NULL AUTO_INCREMENT, user_name varchar(50) NOT NULL UNIQUE, fullname varchar(50) NOT NULL, password varchar(50) NOT NULL, phone_number varchar(11) NOT NULL, email varchar(50) NOT NULL, address varchar(100) NOT NULL, date_of_creation date NOT NULL, status tinyint(3) NOT NULL, PRIMARY KEY (id));
-CREATE TABLE Product (id bigint(19) NOT NULL AUTO_INCREMENT, product_name varchar(50) NOT NULL UNIQUE, price double NOT NULL, descriptions text NOT NULL, status int(10) NOT NULL, available int(10) NOT NULL, images text NOT NULL, quantity int(10) NOT NULL, Categoryid bigint(20) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE `user` (id bigint(19) NOT NULL AUTO_INCREMENT, user_name varchar(50) NOT NULL UNIQUE, fullname varchar(50) NOT NULL, password varchar(50) NOT NULL, phone_number varchar(11) NOT NULL, email varchar(50) NOT NULL, address varchar(100), PRIMARY KEY (id));
+CREATE TABLE Product (id bigint(19) NOT NULL AUTO_INCREMENT, product_name varchar(50) NOT NULL UNIQUE, price double NOT NULL, descriptions text NOT NULL, images text NOT NULL, quantity int(10) NOT NULL, Categoryid bigint(20) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Orders_detail (quantity int(11), price double, Productid bigint(19) NOT NULL, Ordersid bigint(19) NOT NULL, PRIMARY KEY (Productid, Ordersid));
 CREATE TABLE Category (id bigint(20) NOT NULL AUTO_INCREMENT, category_name varchar(30) NOT NULL, descriptions text, parent_category_id bigint(20) DEFAULT NULL, PRIMARY KEY (id));
 CREATE TABLE Orders (id bigint(19) NOT NULL AUTO_INCREMENT, user_id bigint(19) NOT NULL, order_time datetime NOT NULL, delivery_time datetime NOT NULL, status text, Address text, payment_id bigint(20) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Payment_method (id bigint(20) NOT NULL AUTO_INCREMENT, name varchar(20) NOT NULL, user_id bigint(19) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Cart (id bigint(20) NOT NULL AUTO_INCREMENT, user_id bigint(19) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Cart_item (id bigint(20) NOT NULL, Cartid bigint(20) NOT NULL, Productid bigint(19) NOT NULL, quantity int(11), PRIMARY KEY (id, Cartid, Productid));
+CREATE TABLE Color (id bigint(20) NOT NULL AUTO_INCREMENT, product_id bigint(19) NOT NULL, color varchar(10) NOT NULL, PRIMARY KEY (id));
 ALTER TABLE Orders ADD CONSTRAINT FKOrders435482 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE Category ADD CONSTRAINT FKCategory177379 FOREIGN KEY (parent_category_id) REFERENCES Category (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE Product ADD CONSTRAINT FKProduct367954 FOREIGN KEY (Categoryid) REFERENCES Category (id) ON DELETE CASCADE ON UPDATE CASCADE;
@@ -16,3 +17,4 @@ ALTER TABLE Orders_detail ADD CONSTRAINT FKOrders_det654225 FOREIGN KEY (Ordersi
 ALTER TABLE Orders_detail ADD CONSTRAINT FKOrders_det937939 FOREIGN KEY (Productid) REFERENCES Product (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE Payment_method ADD CONSTRAINT FKPayment_me33431 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE Orders ADD CONSTRAINT FKOrders552269 FOREIGN KEY (payment_id) REFERENCES Payment_method (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Color ADD CONSTRAINT FKColor487049 FOREIGN KEY (product_id) REFERENCES Product (id) ON DELETE CASCADE ON UPDATE CASCADE;
