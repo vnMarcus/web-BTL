e5b058c7f7b8ad720323a2a84e6f4d0113035c5d
Jhonaxth <111892216+Jhonaxth@users.noreply.github.com>
2023-05-02 10:10:38 +0700

    Add files via upload

--- LaptrinhWeb/script.js
+++ LaptrinhWeb/script.js
@@ -93,4 +93,13 @@ function imageZoom(imgID){
 }
 
 imageZoom('featured')
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.body.style.backgroundColor = "white";
+  }
 
